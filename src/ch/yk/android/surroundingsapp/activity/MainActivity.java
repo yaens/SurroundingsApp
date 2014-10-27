eb21694e1dcb2ccb752c736f6e5edcdd5e9c0faa
yaens <yannik@kopp.ch>
2014-10-28 00:53:04 +0100

    added input field to add address

--- src/ch/yk/android/surroundingsapp/activity/MainActivity.java
+++ src/ch/yk/android/surroundingsapp/activity/MainActivity.java
@@ -1,43 +1,20 @@
 package ch.yk.android.surroundingsapp.activity;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import android.os.AsyncTask;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 import ch.yk.android.surroundingsapp.R;
-import ch.yk.android.surroundingsapp.R.id;
-import ch.yk.android.surroundingsapp.R.layout;
-import ch.yk.android.surroundingsapp.R.menu;
 import ch.yk.android.surroundingsapp.RESTResult.Musikschule;
 import ch.yk.android.surroundingsapp.rest.CallAPI;
 
@@ -46,7 +23,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends FragmentActivity implements OnTaskCompleted{
+public class MainActivity extends FragmentActivity implements OnTaskCompleted {
 
 	private GoogleMap mMap;
 
@@ -55,6 +32,30 @@ public class MainActivity extends FragmentActivity implements OnTaskCompleted{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		setUpMapIfNeeded();
+		EditText editText = (EditText) findViewById(R.id.input_address);
+		
+		
+		
+		editText.setOnEditorActionListener(new OnEditorActionListener() {
+		    @Override
+		    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+		        boolean handled = false;
+		        if (actionId == EditorInfo.IME_ACTION_DONE) {
+		        	
+		        	//Hide Keyboard after the ok button click
+		        	v.clearFocus();
+		        	EditText myEditText = (EditText) findViewById(R.id.input_address);  
+		        	InputMethodManager imm = (InputMethodManager)getSystemService(
+		        	      Context.INPUT_METHOD_SERVICE);
+		        	imm.hideSoftInputFromWindow(myEditText.getWindowToken(), 0);
+		        	
+		            handled = true;
+		            
+		            setUpMap();
+		        }
+		        return handled;
+		    }
+		});
 	}
 
 	@Override
@@ -85,27 +86,26 @@ public class MainActivity extends FragmentActivity implements OnTaskCompleted{
 					.findFragmentById(R.id.map)).getMap();
 			// Check if we were successful in obtaining the map.
 			if (mMap != null) {
-				setUpMap();
+				//setUpMap();
 			}
 		}
 	}
 
 	private void setUpMap() {
-		
+
 		new CallAPI(this).execute();
 
 	}
 
 	@Override
 	public void onTaskCompleted(ArrayList<Musikschule> musikschuleResult) {
-		
-		for(Musikschule elem : musikschuleResult){
-			mMap.addMarker(new MarkerOptions().position(new LatLng(elem.getLat(), elem.getLon())).title(
+
+		for (Musikschule elem : musikschuleResult) {
+			mMap.addMarker(new MarkerOptions().position(
+					new LatLng(elem.getLat(), elem.getLon())).title(
 					elem.getName()));
 		}
-			   
-		
+
 	}
-	
 
 }
